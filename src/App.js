a8b0c0a843f30b1a0bd0ee8f2e2c7f597022afff
optus-56 <bijanbakabal45@gmail.com>
2024-07-31 16:36:30 +0545

    added some images

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [userChoice, setUserChoice] = useState("rock")
+  const [botChoice, setBotChoice] = useState("rock")
+  const [userPoints, setUserPoints] = useState(0)
+  const [botPoints, setBotPoints] = useState(0)
+  const [result, showResult] = useState("")
+  const [gameOver, isGameOver] = useState(false)
+
   return (
     <div className="App">
+      <h1 className="title"> Rock, Paper, Scissors</h1>
+      <div className="points">
+        <h2> User Points : </h2>
+        <h2> Bot Points: </h2>
+      </div>
       
     </div>
   );
